6e7effffab4a3963d5e60ffc5ff73771f63a91c5
Prajwal <prajwal2079@gmail.com>
2025-04-15 23:16:01 +0530

    Updates

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const UAParser = require('ua-parser-js');
 const requestIp = require('request-ip');
+const axios = require('axios');
 
 const app = express();
-const PORT = 3782;
+const PORT = process.env.PORT || 3782;
 
 app.set('view engine', 'ejs');
-app.set('views',"./Views");
+app.set('views', 'Views');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const userAgent = req.headers['user-agent'];
   const parser = new UAParser(userAgent);
   const result = parser.getResult();
@@ -18,12 +20,19 @@ app.get('/', (req, res) => {
     : 'Unknown Device';
 
   const ip = requestIp.getClientIp(req);
-  console.log('Device - ', deviceInfo);
-  console.log('Ip - ', ip);
-  res.render('index', {
-    device: deviceInfo,
-    ip: ip
-  });
+
+  const message = `👀 New Visitor!\nDevice: ${deviceInfo}\nIP: ${ip}`;
+
+  try {
+    await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+      chat_id:  process.env.TELEGRAM_CHAT_ID,
+      text: message
+    });
+  } catch (error) {
+    console.error('Failed To Send Telegram Notification:', error.response ? error.response.data : error.message);
+  }
+
+  res.render('index', { device: deviceInfo, ip: ip });
 });
 
 app.listen(PORT, () => {
